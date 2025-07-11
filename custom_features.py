cd705a03e1150192f1696c7f1b6a9347d6d8478d
POOJA 230701226 <230701226@rajalakshmi.edu.in>
2025-07-11 21:50:16 +0530

    Add files via upload

---
+++ custom_features.py
@@ -0,0 +1,14 @@
+# custom_features.py
+
+import re
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class TextLengthExtractor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None): return self
+    def transform(self, X): return np.array([[len(text)] for text in X])
+
+class LinkCountExtractor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None): return self
+    def transform(self, X):
+        return np.array([[len(re.findall(r'http[s]?://', text))] for text in X])
